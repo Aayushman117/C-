1ea79303a9326b36a2a2809589c0e77703ea9e71
Aayushman117 <aayushmansemwal117@gmail.com>
2025-07-12 21:34:54 +0530

    First commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string s1(n,'c')
+    cout<<s1<<endl;
+}
+
+
+SHA256:xxbhAGQp6G0FwTAKm+RoN7kJl290qd6MAY6C25700WU aayushmansemwal117@gmail.com
